102d6021d6af7b50fafd2f88832100ee59cab872
Nadezhda <nbykova79@gmail.com>
2022-06-28 06:22:56 +0300

    обновили файл Program

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// выдает все четные числа от 1 до n
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0)
+{
+    int index = 2;
+    while (index <= number)
+    {
+        Console.WriteLine(index);
+        index = index + 2;
+    }
+}
+
+if (number < 0)
+{
+    int index = -2;
+    while (index >= number)
+    {
+        Console.WriteLine(index);
+        index = index - 2;
+    }
+}
+
+if (number == 0)
+{
+   Console.WriteLine("вы ввели ноль");
+}
+
